301a681b95e6fd6d90bb823dbc048304a7a23adc
AlanTai <rainman.tai@gmail.com>
2015-03-08 19:08:10 -0700

    update

--- my_synaptic.js
+++ my_synaptic.js
@@ -4,11 +4,20 @@ GLOBAL.my_synaptic = require('synaptic');
 /* init NN */
 GLOBAL.my_neural_network = GLOBAL.my_neural_network || {};
 
-my_neural_network.neuron = my_synaptic.Neuron,
-my_neural_network.layer = my_synaptic.Layer,
-my_neural_network.network = my_synaptic.Network,
-my_neural_network.Trainer = my_synaptic.Trainer,
-my_neural_network.Architect = my_synaptic.Architect;
+my_neural_network.init_synpatic = function(){
+	my_neural_network.neuron = my_synaptic.Neuron,
+	my_neural_network.layer = my_synaptic.Layer,
+	my_neural_network.network = my_synaptic.Network,
+	my_neural_network.Trainer = my_synaptic.Trainer,
+	my_neural_network.Architect = my_synaptic.Architect;
+	
+	console.log("Successfully Initiate neural-network elements");
+}
 
 //
-my_neural_network.Perceptron = function(){}
+my_neural_network.Perceptron = function(){
+	// create layers
+}
+
+// start action
+my_neural_network.init_synpatic();
