ca0cb615a543b79e662a3b98df1b5a9d19cfb377
AlanTai <rainman.tai@gmail.com>
2015-03-08 19:28:03 -0700

    update synaptic

--- my_synaptic.js
+++ my_synaptic.js
@@ -11,12 +11,28 @@ my_neural_network.init_synpatic = function(){
 	my_neural_network.Trainer = my_synaptic.Trainer,
 	my_neural_network.Architect = my_synaptic.Architect;
 	
+	my_neural_network.Perceptron = function(input, hidden, output){
+		// create layers
+		var input_layer = new my_neural_network.layer(input),
+			hidden_layer = new my_neural_network.layer(input),
+			output_layer = new my_neural_network.layer(output);
+		
+		// connect layers
+		input_layer.project(hidden_layer);
+		hidden_layer.project(output_layer);
+		
+		// set the layers
+		this.set({
+			input : input_layer,
+			hidden: [hidden_layer],
+			output: output_layer
+		});
+	}
+	
+	my_neural_network.Perceptron.prototype = new my_neural_network.network();
+	my_neural_network.Perceptron.prototype.constructor = my_neural_network.Perceptron;
+	
 	console.log("Successfully Initiate neural-network elements");
-}
-
-//
-my_neural_network.Perceptron = function(){
-	// create layers
 }
 
 // start action
