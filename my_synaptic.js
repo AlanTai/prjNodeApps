6c2f88cf8605b918fb54abcc04fb00fd0dbe5c72
AlanTai <rainman.tai@gmail.com>
2015-03-08 19:38:02 -0700

    update

--- my_synaptic.js
+++ my_synaptic.js
@@ -8,8 +8,8 @@ my_neural_network.init_synpatic = function(){
 	my_neural_network.neuron = my_synaptic.Neuron,
 	my_neural_network.layer = my_synaptic.Layer,
 	my_neural_network.network = my_synaptic.Network,
-	my_neural_network.Trainer = my_synaptic.Trainer,
-	my_neural_network.Architect = my_synaptic.Architect;
+	my_neural_network.trainer = my_synaptic.Trainer,
+	my_neural_network.architect = my_synaptic.Architect;
 	
 	my_neural_network.Perceptron = function(input, hidden, output){
 		// create layers
@@ -29,11 +29,26 @@ my_neural_network.init_synpatic = function(){
 		});
 	}
 	
+	// set perceptron
 	my_neural_network.Perceptron.prototype = new my_neural_network.network();
 	my_neural_network.Perceptron.prototype.constructor = my_neural_network.Perceptron;
 	
 	console.log("Successfully Initiate neural-network elements");
+	
+	//
+	my_neural_network.test_perceptron = function(){
+		var my_perceptron = new my_neural_network.perceptron(2,3,1);
+		var my_trainer = new my_neural_network.trainer(my_perceptron);
+		
+		my_trainer.XOR();
+		
+		my_perceptron.activate([0,0]);
+		my_perceptron.activate([1,0]);
+		my_perceptron.activate([0,1]);
+		my_perceptron.activate([1,1]);
+	}
 }
 
 // start action
 my_neural_network.init_synpatic();
+my_neural_network.test_perceptron();
