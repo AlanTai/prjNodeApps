1d2bb49b6dac6d6337395df3d423904dede6192e
AlanTai <rainman.tai@gmail.com>
2015-03-08 19:01:36 -0700

    update NN

--- my_synaptic.js
+++ my_synaptic.js
@@ -1,5 +1,14 @@
+/* require libs */
 GLOBAL.my_synaptic = require('synaptic');
 
+/* init NN */
 GLOBAL.my_neural_network = GLOBAL.my_neural_network || {};
 
-my_neural_network.neuron = my_synaptic.Neuron;
+my_neural_network.neuron = my_synaptic.Neuron,
+my_neural_network.layer = my_synaptic.Layer,
+my_neural_network.network = my_synaptic.Network,
+my_neural_network.Trainer = my_synaptic.Trainer,
+my_neural_network.Architect = my_synaptic.Architect;
+
+//
+my_neural_network.Perceptron(){}
