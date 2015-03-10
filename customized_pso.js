df4337317ae33b631597064b5c853bba0709fdd0
AlanTai <rainman.tai@gmail.com>
2015-03-09 19:30:31 -0700

    update

--- customized_pso.js
+++ customized_pso.js
@@ -260,12 +260,12 @@ GLOBAL.particles_swarm_optimization = GLOBAL.particles_swarm_optimization || {
 // GLOBAL.particles_swarm_optimization.start_optimization();
 
 GLOBAL.customized_optimization = GLOBAL.customized_optimization || {};
-GLOBAL.customized_optimization.performance_index = function(arg_x, arg_y, arg_z){
+GLOBAL.customized_optimization.performance_index = function(arg_x, arg_y, arg_z, arg_a){
 	/* Performance index for Demo:
 	 * sin(arg_x) * cos(arg_y + 0.5 * arg_x) + arg_z^2 * cos(arg_x + arg_y * arg_z); */
 	return (Math.sin(arg_x) * Math.cos(arg_y + 0.5 * arg_x) + Math.pow(arg_z, 2) * Math.cos(arg_x + arg_y * arg_z));
 };
 
-GLOBAL.customized_optimization.variable_ranges = [[-10, 20, -15], [30, 50, 25]];
+GLOBAL.customized_optimization.variable_ranges = [[-10, 20, -15, 5], [30, 50, 25, 70]];
 GLOBAL.particles_swarm_optimization.set_customized_performance_index_and_variable_ranges(GLOBAL.customized_optimization.performance_index, GLOBAL.customized_optimization.variable_ranges);
 GLOBAL.particles_swarm_optimization.start_optimization();
