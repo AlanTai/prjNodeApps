5f8efec00eed0d72e7db40050807b912c4eb43a6
AlanTai <rainman.tai@gmail.com>
2015-03-09 18:02:27 -0700

    update

--- customized_pso.js
+++ customized_pso.js
@@ -208,7 +208,10 @@ GLOBAL.particles_swarm_optimization = GLOBAL.particles_swarm_optimization || {
 					}
 					this.config.performance_index_set.push(performance_index);
 				}
-				
+				console.log(this.config.performance_index_set);
+				if(ith == 2){
+				return;
+				}
 				// temp cost
 				var temp_cost_set = numeric.mul(- this.config.max_min_factor, this.config.performance_index_set);
 				
