16b37adef25648664c53e72dfe4e90c4fa8186e4
AlanTai <rainman.tai@gmail.com>
2015-04-01 16:31:25 -0700

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ GLOBAL.server = http.createServer(function(req, res){
 			});
 			break;
 		case '/check_status':
-			res.write({msg : 'hello node.js'});
+			res.write(JSON.stringify({msg : 'hello node.js'}));
 			break;
 		default:
 			res.writeHead(404);
